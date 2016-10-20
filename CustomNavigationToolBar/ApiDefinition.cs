94319e5ab260b3f9bd7ea893efe52a0b8a51a90f
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-10-20 15:09:38 +0530

    Custom Navigation Tool Bar
    Custom Navigation Tool Bar

---
+++ CustomNavigationToolBar/ApiDefinition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace CustomNavigationToolBar
+{
+	// @interface CustomNavigationController : UINavigationController
+	[BaseType(typeof(UINavigationController))]
+	interface CustomNavigationController
+	{
+		// @property (nonatomic, strong) UIToolbar * dropDownToolbar;
+		[Export("dropDownToolbar", ArgumentSemantic.Strong)]
+		UIToolbar DropDownToolbar { get; set; }
+
+		// @property (nonatomic, strong) NSString * activeNavigationBarTitle;
+		[Export("activeNavigationBarTitle", ArgumentSemantic.Strong)]
+		string ActiveNavigationBarTitle { get; set; }
+
+		// @property (nonatomic, strong) NSString * activeBarButtonTitle;
+		[Export("activeBarButtonTitle", ArgumentSemantic.Strong)]
+		string ActiveBarButtonTitle { get; set; }
+
+		// @property (assign, nonatomic) BOOL isDropDownVisible;
+		[Export("isDropDownVisible")]
+		bool IsDropDownVisible { get; set; }
+
+		// -(void)toggleDropDown:(id)sender;
+		[Export("toggleDropDown:")]
+		void ToggleDropDown(NSObject sender);
+
+		// -(void)hideDropDown:(id)sender;
+		[Export("hideDropDown:")]
+		void HideDropDown(NSObject sender);
+
+		// -(void)showDropDown:(id)sender;
+		[Export("showDropDown:")]
+		void ShowDropDown(NSObject sender);
+	}
+}
---
+++ Sample/ViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CustomNavigationToolBar;
+using UIKit;
+using Foundation;
+
+namespace Sample
+{
+	public partial class ViewController : UIViewController
+	{
+		public CustomNavigationController NavController {
+			get;
+			set;
+		}
+
+		public ViewController(IntPtr handle) : base(handle)
+		{
+
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			this.View.BackgroundColor = UIColor.White;
+			this.Title = "Custom Navigation Bar";
+			if (this.NavigationController != null
+				&& this.NavigationController is CustomNavigationController) {
+				NavController = (CustomNavigationController)this.NavigationController;
+				NavController.ActiveNavigationBarTitle = "Choose Menu Items";
+				this.NavigationItem.RightBarButtonItem = new UIBarButtonItem(UIBarButtonSystemItem.Add, DidSelectShow);
+			}
+		}
+
+		public void DidSelectShow(object sender, EventArgs evt)
+		{
+			this.NavController.DropDownToolbar.Items = new UIBarButtonItem[]
+			{
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Edit),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Save),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Pause),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Compose),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Camera),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Reply),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				new UIBarButtonItem(UIBarButtonSystemItem.Refresh),
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace)
+			};
+			var sendObj = sender as UIBarButtonItem;
+			if (this.NavController.IsDropDownVisible) {
+				this.NavController.HideDropDown(sendObj);
+				this.Title = "Custom Navigation Bar";
+			} else {
+				this.NavController.ShowDropDown(sendObj);
+			}
+		}
+	}
+}
